1effabe8f78ccb740bb2400ebfd172042d8bc49d
Firesidee <KathrynY@protonmail.com>
2021-11-02 19:19:04 -0600

    commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+// Causes an alert to appear on the web page - needs to be closed by the user
+window.alert("Why am I here?");
+// Displays "I don't know" on the webpage after the alert is closed
+document.write("I don't know");
+// Assignes the value "Waddles" to the variable "myTurtle"
+let myTurtle = "Waddles";
+// Displays "Waddles" on the webpage
+document.write(myTurtle);
+let myJob = "Stall Cleaner";
+window.alert(myJob);
+// Allows for apostrophes to be displayed on the webpage 
+document.write("\ Let's\ go to \ Sally's\ house!");
+document.write("Chickens" + " " + "are" + " " + "awesome");
+// Concatenated variable
+let fowl = " " + "chickens" + " " + "and" + " " + "ducks";
+document.write(fowl);
+// Multiple values assigned to one variable
+var dogs = "The Pack", Casper = "white male", 
+Bella = "brown and white female", Spot = "brown and white male", Myron = "black male";
+// Changes the color of the main variable and/or the individual values
+var dogs = dogs.fontcolor("purple");
+var Spot = Spot.fontcolor("blue");
+var Bella = Bella.fontcolor("pink");
+var Myron = Myron.fontcolor("red");
+var Casper = Casper.fontcolor("green");
+document.write(Spot);
+let problem = " 7 + 15";
+document.write(problem);
+
+
