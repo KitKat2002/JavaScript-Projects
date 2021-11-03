7090aed3eb511921e0fd133ec2bdb89bbafed475
Firesidee <KathrynY@protonmail.com>
2021-11-03 11:06:38 -0600

    commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+// Creates a function named "Chicken"
+function Chicken() {
+// Creates the variable "firstChicken"
+let firstChicken = "Gobblette";
+//Concatenates a string
+firstChicken += "is a 3.5 month old Cornish Rock"
+// Prints the concatenated string
+document.getElementById("Concatenate").innerHTML = firstChicken;
+};
